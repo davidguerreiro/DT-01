5ab6882d841ec65680072abebfc45a9733630000
davidguerreiro <me@davidguerreiro.com>
2020-07-09 02:35:21 +0100

    fix rotation itself script use quaternions instead of euler angles

--- Animations/Generic/RotateItself.cs
+++ Animations/Generic/RotateItself.cs
@@ -7,12 +7,6 @@ public class RotateItself : MonoBehaviour {
     public bool rotateX = true;
     public bool rotateY = true;
     public bool rotateZ = true;
-    private Vector3 rotation;                                   // Rotation vector.
-
-    // Start is called before the first frame update
-    void Start() {
-        // Init();
-    }
 
     // Update is called once per frame
     void Update() {
@@ -23,31 +17,12 @@ public class RotateItself : MonoBehaviour {
     /// Rotate object over itself.
     /// </summary>
     private void RotateObject() {
-        rotation = transform.rotation.eulerAngles;
 
         float rotationValue = speed * Time.deltaTime;
+        float rotateXValue = ( this.rotateX ) ? rotationValue : 0f;
+        float rotateYValue = ( this.rotateY ) ? rotationValue : 0f;
+        float rotateZvalue = ( this.rotateZ ) ? rotationValue : 0f;
 
-        if ( this.rotateX ) {
-            rotation = new Vector3( rotation.x += rotationValue, rotation.y, rotation.z );
-        }
-
-        if ( this.rotateY ) {
-            rotation = new Vector3( rotation.x, rotation.y += rotationValue, rotation.z );
-        }
-
-        if ( this.rotateZ ) {
-            rotation = new Vector3( rotation.x, rotation.y, rotation.z += rotationValue );
-        }
-
-        transform.localEulerAngles = rotation;
-    }
-
-    /// <summary>
-    /// Init class method.
-    /// </summary>
-    private void Init() {
-        
-        // init rotation vector.
-        rotation = Vector3.zero;
+        transform.Rotate( new Vector3( rotateXValue, rotateYValue, rotateZvalue ) );
     }
 }
