1beb3d590ffb212614af4952f96f269185cbf7bf
davidguerreiro <me@davidguerreiro.com>
2020-07-10 00:38:30 +0100

    add utilities script and quit game funcionality to use when testing builds

---
+++ Utilities/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        
+        // quit game when pressing escape key.
+        if ( Input.GetKeyDown( "escape" ) ) {
+            QuitGame();
+        }
+    }
+
+    /// <summary>
+    /// Quite game.
+    /// </summary>
+    private void QuitGame() {
+        Application.Quit();
+    }
+}
