2b3953a42210137297e6525e80fb080aa500a3bc
davidguerreiro <me@davidguerreiro.com>
2020-07-10 01:50:55 +0100

    add walking sound funcionality

--- Player/GroundChecker.cs
+++ Player/GroundChecker.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class GroundChecker : MonoBehaviour {
 
+    public enum WalkingSurfaces {                                    // Walking surfaces enum list - used to display player walking sound.
+        Ground = 0,
+        Metal = 1,
+    };
+
     private FPSInput _playerInput;                                   // Player input class.
+    public WalkingSurfaces currentSurface;                          // Current surface the player is walking into. Used to display different sound when walking.
+    private AudioComponent _audioComponent;                          // Audio component reference.
 
     // Start is called before the first frame update
     void Start() {
@@ -12,18 +19,10 @@ public class GroundChecker : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
-    }
-
-    /// <summary>
-    /// Init class method.
-    /// </summary>
-    private void Init() {
-
-        // get player input script component reference.
-        _playerInput = GetComponentInParent<FPSInput>();
+        // play walking sound if player is moving.
+        PlayWalkingSound();
     }
 
     private void OnTriggerExit(Collider other) {
@@ -38,14 +37,75 @@ public class GroundChecker : MonoBehaviour {
     /// <param name="other">The other Collider involved in this collision.</param>
     void OnTriggerEnter( Collider other ) {
 
+        // check if player is grounded.
         if ( other.gameObject.layer == LayerMask.NameToLayer( "Ground" ) ) {
             _playerInput.grounded = true;
+
+            // check to which surface the player is grounded.
+            CheckSurfaceType( other.gameObject );
         }
     }
 
     private void OnTriggerStay(Collider other) {
+
+        // check if player is grounded.
          if ( other.gameObject.layer == LayerMask.NameToLayer( "Ground" ) ) {
             _playerInput.grounded = true;
+
+            // check to which surface the player is grounded.
+            CheckSurfaceType( other.gameObject );
         }   
+    }
+
+    /// <summary>
+    /// Check surface type.
+    /// Used to play different sound when moving.
+    /// </summary>
+    /// <param name="surfaceObject">GameObject - surface game object</param>
+    private void CheckSurfaceType( GameObject surfaceObject ) {
+
+        switch ( surfaceObject.tag ) {
+            case "GroundSurface":
+                currentSurface = WalkingSurfaces.Ground;
+                break;
+            case "MetalSurface":
+                currentSurface = WalkingSurfaces.Metal;
+                break;
+            default:
+                currentSurface = WalkingSurfaces.Ground;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Play walking sound.
+    /// </summary>
+    private void PlayWalkingSound() {
+
+        if ( _playerInput.grounded && _playerInput.isMoving ) {
+
+            if ( ! _audioComponent.audio.isPlaying ) {
+
+                int surfaceSoundKey = (int) currentSurface;
+                _audioComponent.PlaySound( surfaceSoundKey );
+            }
+        } else {
+            _audioComponent.PauseAudio();
+        }
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+
+        // get player input script component reference.
+        _playerInput = GetComponentInParent<FPSInput>();
+
+        // get audio component.
+        _audioComponent = GetComponent<AudioComponent>();
+
+        // set default value for walking surface.
+        currentSurface = WalkingSurfaces.Ground;
     }
 }
--- Utilities/AudioComponent.cs
+++ Utilities/AudioComponent.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class AudioComponent : MonoBehaviour {
     public AudioClip[] audioClips;                              // Audioclips to use with this audio component instance.
-    private AudioSource audio;                                  // Audio component reference.
+    
+    [HideInInspector]
+    public AudioSource audio;                                  // Audio component reference.
     private int currentAudioClipIndex;                          // Current audio clip index assigned to the audio clip.
 
     // Start is called before the first frame update
@@ -17,10 +19,6 @@ public class AudioComponent : MonoBehaviour {
     /// </summary>
     /// <param name="soundClip">int - sound clip to play from the array of sounds.</param>
     public void PlaySound( int audioClip ) {
-
-        if ( audio.isPlaying ) {
-            audio.Stop();
-        }
 
         audio.clip = audioClips[ audioClip ];
         this.currentAudioClipIndex = audioClip;
@@ -35,10 +33,6 @@ public class AudioComponent : MonoBehaviour {
     /// <param name="clip">AudioClip - new audio clip to play.</param>
     /// <param name="autoPlay">bool - true by default. Wheter to play the audio now or later</param>
     public void PlayClip( AudioClip clip, bool autoPlay = true ) {
-
-        if ( audio.isPlaying && autoPlay ) {
-            audio.Stop();
-        }
 
         audio.clip = clip;
 
@@ -63,6 +57,13 @@ public class AudioComponent : MonoBehaviour {
     /// </summary>
     public void StopAudio() {
         audio.Stop();
+    }
+
+    /// <summary>
+    /// Pause audio.
+    /// </summary>
+    public void PauseAudio() {
+        audio.Pause();
     }
 
     /// <summary>
