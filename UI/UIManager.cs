3bb227723f62288b8dbc3c49244fffcf40a1a520
davidguerreiro <me@davidguerreiro.com>
2020-07-10 18:17:50 +0100

    add object pooling for weapon shooting

--- UI/UIManager.cs
+++ UI/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
     public static UIManager instance;                           // Public static class instance - this class must be instantiated just once.
+    public bool cursorEnabled;                                  // Wheter the cursor is enabled or disabled in the game.
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -11,6 +12,27 @@ public class UIManager : MonoBehaviour {
     void Awake() {
         if ( instance == null ) {
             instance = this;
+        }
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update() {
+        
+    }
+
+    /// <summary>
+    /// Update cursor based on public
+    /// attribute. This attribute is modified
+    /// by game logic through the game.
+    /// </summary>
+    private void CheckCursor() {
+
+        if ( cursorEnabled ) {
+            DisplayCursor();
+        } else {
+            HideCursor();
         }
     }
 
--- Weapons/PlasmaGun/MainWeapon.cs
+++ Weapons/PlasmaGun/MainWeapon.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class MainWeapon : MonoBehaviour {
     public FPSInput player;                                     // Player controller class.
+    
+    [Header( "Weapon Animation" ) ]
     public float idleSpeed = 0.2f;                              // Idle animation speed.
     public float idleWalkingSpeed = 0.7f;                       // Idle animtion walking speed.
     public float idleRunningSpeed = 2f;                         // Idle animation running speed.
     private Animator animator;                                  // Animator component reference.
+
+    // shooting object pool variables.
+    [Header( "Shooting Ammo" ) ]
+    public GameObject ammoPrefab;                               // Ammo proyectile prefab to instantiate when shooting.
+    public GameObject shootingOrigin;                           // Shooting origin - point from where the proyectiles are shoot from the weapon. 
+    private static List<GameObject> ammoPool;                   // Ammo pool list of gameObjects - used to save a ready-to-use pool of ammo objects to shoot.
+    public int poolSize;                                        // Number of ammo obejcts to keep in the object pool.
+    
+
     [HideInInspector]
     public enum AnimStates {                                    // Machine states for the animation.
         Stopped,
@@ -17,6 +28,15 @@ public class MainWeapon : MonoBehaviour {
 
     [HideInInspector]
     public AnimStates animStates;                              // Instance for animation states.
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake() {
+
+        // set ammo pool for player projectiles shooting.
+       SetAmmoPool();
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -68,6 +88,57 @@ public class MainWeapon : MonoBehaviour {
                 break;
         }
     }
+
+    /// <summary>
+    /// Create ammo pool.
+    /// Object pool approach is being taken
+    /// to shoot proyectiles when shooting the 
+    /// weapon, so we avoid instantiate / destroy
+    /// objects operations and improve performance.
+    /// </summary>
+    private void SetAmmoPool() {
+
+        if ( ammoPool == null ) {
+            ammoPool = new List<GameObject>();
+        }
+
+        for ( int i = 0; i < poolSize; i++ ) {
+
+            GameObject ammoObject = Instantiate( ammoPrefab );
+            ammoObject.transform.parent = shootingOrigin.transform;
+            ammoObject.transform.localPosition = Vector3.zero;
+
+            ammoObject.SetActive( false );
+            ammoPool.Add( ammoObject );
+        }
+    }
+
+    /// <summary>
+    /// Spawn ammo.
+    /// </summary>
+    /// <returns>GameObject</returns>
+    public GameObject SpawnAmmo() {
+
+        foreach ( GameObject ammo in ammoPool ) {
+            
+            if ( ammo.activeSelf == false ) {
+
+                // check if it is a already shot projectile.
+                if ( ammo.transform.parent == null ) {
+                    ammo.transform.parent = shootingOrigin.transform;
+                    ammo.transform.localPosition = Vector3.zero;
+                }
+
+                ammo.SetActive( true );
+
+                return ammo;
+            }
+
+        }
+        
+        return null;
+    }
+
 
     /// <summary>
     /// Init class method.
