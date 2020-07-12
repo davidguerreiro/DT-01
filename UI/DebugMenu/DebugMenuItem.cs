abb31087cfa02c61b12a9aa5c94ab4841ccd46dd
davidguerreiro <me@davidguerreiro.com>
2020-07-12 20:00:13 +0100

    complete debug menu

---
+++ UI/DebugMenu/DebugMenuItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMenuItem : MonoBehaviour {
+    private Animator _animator;                                     // Animator component reference.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Hover item.
+    /// </summary>
+    public void Hover() {
+        Debug.Log( "hover" );
+        _animator.SetBool( "Hover", true );
+    }
+
+    /// <summary>
+    /// Unhover item.
+    /// </summary>
+    public void UnHover() {
+        Debug.Log( "unhover" );
+        _animator.SetBool( "Hover", false );
+    }
+
+    /// <summary>
+    /// Load scene when the
+    /// item is clicked.
+    /// </summary>
+    public void LoadScene() {
+
+        SceneLoader sceneLoader = GetComponent<SceneLoader>();
+
+        if ( sceneLoader != null ) {
+            sceneLoader.LoadScene();
+        }
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+
+        // get animator componentr.
+        _animator = GetComponent<Animator>();
+    }
+}
