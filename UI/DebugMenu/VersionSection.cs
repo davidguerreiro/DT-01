5df96e0239440b3ac49666cfb0dbe054cf709d78
davidguerreiro <me@davidguerreiro.com>
2020-07-12 14:26:15 +0100

    add text component and add version section script logic

---
+++ UI/DebugMenu/VersionSection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionSection : MonoBehaviour {
+    public TextComponent buildVersionText;                              // Build version text component class reference.
+    public TextComponent buildDateText;                                 // Build date text component.
+    public BuildInfo buildInfo;                                         // Build info scriptable object instance.
+
+    // Start is called before the first frame update
+    void Start() {
+        UpdateUIValues();
+    }
+
+    /// <summary>
+    /// Update build values
+    /// in the UI.
+    /// </summary>
+    private void UpdateUIValues() {
+        buildVersionText.UpdateContent( buildInfo.version );
+        buildDateText.UpdateContent( buildInfo.lastBuildDate );
+    }
+}
---
+++ Utilities/TextComponent.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextComponent : MonoBehaviour {
+    
+    private Text content;                                          // Text component reference.
+
+    // Start is called before the first frame update
+    void Start() { 
+        Init();
+    }
+
+    /// <summary>
+    /// Get current text
+    /// content.
+    /// </summary>
+    /// <returns>string</returns>
+    public string GetContent() {
+        return content.text;
+    }
+
+    /// <summary>
+    /// Update content.
+    /// </summary>
+    /// <param name="newContent">string - new content to be displayed in this text component.</param>
+    public void UpdateContent( string newContent ) {
+
+        if ( content == null ) {
+            content = GetComponent<Text>();
+        }
+
+        content.text = newContent;
+    }
+
+    /// <summary>
+    /// Update text colour.
+    /// </summary>
+    /// <param name="colour">color - Colour to apply to the text</param>
+    public void UpdateColour( Color colour ) {
+        content.color = colour;
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    public void Init() {
+
+        // get text component reference.
+        if ( content == null ) {
+            content = GetComponent<Text>();
+        }
+    }
+}
