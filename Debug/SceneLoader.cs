b9824993e07c13ecd14ce453093874d7544f15dc
davidguerreiro <me@davidguerreiro.com>
2020-07-12 13:11:46 +0100

    add load scene script

---
+++ Debug/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public string sceneToLoad;                              // Scene to load by this script.
+
+    /// <summary>
+    /// Load scene by name.
+    /// </summary>
+    public void LoadScene() {
+        SceneManager.LoadScene( sceneToLoad );
+    }
+
+    /// <summary>
+    /// Load scene by name.
+    /// </summary>
+    public void LoadScene( string sceneName ) {
+        SceneManager.LoadScene( sceneName );
+    }
+}
