39b8224b349cb2cfd5465db00c6af5a842e28e26
davidguerreiro <me@davidguerreiro.com>
2020-07-14 19:19:23 +0100

    add fade script to control fade animations in the UI

--- UI/Animations/FadeElement.cs
+++ UI/Animations/FadeElement.cs
@@ -2,17 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FadeElement : MonoBehaviour
-{
+public class FadeElement : MonoBehaviour {
+    public bool displayed;                                  // Flag to control displayed status.
+    private Animator _animator;                             // Animator component reference.
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    /// <summary>
+    /// Fade out element.
+    /// </summary>
+    public void FadeOut() {
         
+        if ( _animator != null ) {
+            _animator.SetTrigger( "FadeOut" );
+            displayed = false;
+        }
     }
+
+    /// <summary>
+    /// Fade out element.
+    /// </summary>
+    /// <param name="speed">float - fade out speed for animation</param>
+    public void FadeOut( float speed ) {
+        
+        if ( _animator != null ) {
+            _animator.SetFloat( "FadeOutSpeed", speed );
+            _animator.SetTrigger( "FadeOut" );
+            displayed = false;
+        }
+    }
+
+    /// <summary>
+    /// Fade in element.
+    /// </summary>
+    public void FadeIn() {
+
+        if ( _animator != null ) {
+            _animator.SetTrigger( "FadeIn" );
+            displayed = true;
+        }
+    }
+
+    /// <summary>
+    /// Fade in element.
+    /// </summary>
+    public void FadeIn( float speed ) {
+
+        if ( _animator != null ) {
+            _animator.SetFloat( "FadeInSpeed", speed );
+            _animator.SetTrigger( "FadeIn" );
+            displayed = true;
+        }
+    }
+
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+
+        // get animator component reference.
+        _animator = GetComponent<Animator>();
+    }
+
 }
