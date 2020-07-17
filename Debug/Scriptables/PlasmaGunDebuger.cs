00446a3c4f433848730dcaa4a789f6e491a270ee
davidguerreiro <me@davidguerreiro.com>
2020-07-17 16:50:29 +0100

    add debbuger script for plasma gun scriptable object

---
+++ Debug/Scriptables/PlasmaGunDebuger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaGunDebuger : MonoBehaviour {
+    public bool restoreToDefault;                               // Restore basic changing values to default.
+    public bool restoreCompletely = false;                      // Restore all changing values to default, including those affected by skills and items.
+    public PlasmaGun plasmaGunData;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        // check if the choosen component needs to be restore to default values.
+        if ( restoreToDefault ) {
+            RestorePlasmaToDefault( restoreCompletely );
+        }
+    }
+
+    
+    /// <summary>
+    /// Restore to default values.
+    /// </summary>
+    /// <parma name="restoreCompletely">bool - wheter to restore the component to real default values ( values before applying skills and items )</param>
+    private void RestorePlasmaToDefault( bool restoreCompletely ) {
+        plasmaGunData.RestartDefaultValues( restoreCompletely );
+    }
+}
