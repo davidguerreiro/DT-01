50e06e6a35cc6db21873733c635b29118fa4e212
davidguerreiro <me@davidguerreiro.com>
2020-08-10 23:00:25 +0100

    add player health component to gameplay UI

--- UI/GamePlay/GamePlayUI.cs
+++ UI/GamePlay/GamePlayUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GamePlayUI : MonoBehaviour {
     public static GamePlayUI instance;                      // Class static instance reference - unique class instance.
     public PlayerDamagedComponent playerDamagedComponent;   // Player damage component wrapper for blood damage elemnts in the gameplay UI.
+    public PlayerHealComponent playerHealComponent;         // Player health component wrapper to be used when the player recovers health.
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -24,6 +25,17 @@ public class GamePlayUI : MonoBehaviour {
         if ( playerDamagedComponent.bloodPanel != null && playerDamagedComponent.bloodImage != null ) {
             playerDamagedComponent.bloodPanel.Display();
             playerDamagedComponent.bloodImage.Display();
+        }
+    }
+
+    /// <summary>
+    /// Display player healing
+    /// animation in the UI.
+    /// </summary>
+    public void PlayerHealth() {
+
+        if ( playerHealComponent.healthPanel != null ) {
+            playerHealComponent.healthPanel.Display();
         }
     }
 }
