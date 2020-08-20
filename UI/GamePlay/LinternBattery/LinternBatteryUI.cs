6f6dec6136a22d03842de7394d02c65d318c3c4d
davidguerreiro <me@davidguerreiro.com>
2020-08-21 00:34:39 +0100

    add counters to lintern UI to check whether the UI has to become transparent and when it has to dissapear

--- UI/GamePlay/LinternBattery/LinternBatteryUI.cs
+++ UI/GamePlay/LinternBattery/LinternBatteryUI.cs
@@ -17,7 +17,14 @@ public class LinternBatteryUI : MonoBehaviour {
     public FadeElement barFill;                             // Battery bar fill. 
     public FadeElement barText;                             // Battery text displayed below the battery bar.
 
+    [Header("Settings")]
+    public int timeToTransparent;                           // How long to wait until the UI becomes transparent when the battery is recharging.
+    public int timeToHide;                                  // How long to wait unitl the UI is hidden after the battery has been fully charged.
+
     private Slider _slider;                                 // Slider UI component reference.
+    private bool _isTransparent = false;                    // Flag to control whether the UI is displayed as transparent.
+    private int _toHalf = 0;                                // Transparent UI internal counter.
+    private int _toHide = 0;                                // Hide lintern UI internal counter.
 
 
     // Start is called before the first frame update.
@@ -28,6 +35,20 @@ public class LinternBatteryUI : MonoBehaviour {
     // Update is called once per frame.
     void Update() {
         
+        // check if the battery bar has to be displayed.
+        if ( ! displayed && linternData.enabled ) {
+            Display();
+        }
+
+        // check if the battery bar has to start filling in.
+        if ( displayed && ! linternData.enabled ) {
+            FillIn();
+        }
+
+        // fill out battery bar if the lintern is in use.
+        if ( linternData.enabled ) {
+            FillOut();
+        }
     }
     
     /// <summary>
@@ -50,6 +71,10 @@ public class LinternBatteryUI : MonoBehaviour {
         barBackground.FadeIn();
         barFill.FadeIn();
         barText.FadeIn();
+
+        _toHalf = 0;
+        _toHide = 0;
+
         displayed = true;
     }
 
@@ -72,6 +97,8 @@ public class LinternBatteryUI : MonoBehaviour {
         barBackground.HalfFadeOut();
         barFill.HalfFadeOut();
         barText.HalfFadeOut();
+
+        _toHalf = 0;
     }
 
     /// <summary>
