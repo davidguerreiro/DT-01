09e4acdb5d012f9ab13e8aa5204ecd64f7f53c20
davidguerreiro <me@davidguerreiro.com>
2020-08-31 19:32:40 +0100

    add abstrac class collectible and add shard collectable class

---
+++ Collectable/Collectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Collectable : MonoBehaviour {
+
+    public enum Type {
+        stackable,
+        nonStackable,
+    };
+
+    public Type type;                                   // Collectible type. Stackable for inventary-type collectables and non-stackable for collecting only items like shards.
+    
+}
---
+++ Collectable/Shard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shard : Collectable {
+    public PlayerStats playerStats;                     // Data source, used to update player amount of shards.
+    public int value;                                   // How many shards collected by this shard.
+    public GameObject model;                            // 3D shard model.
+
+    /// <summary>
+    /// Collected.
+    /// Update player's total number of shards.
+    /// </summary>
+    public void Collect() {
+        DisableModels();
+
+        playerStats.UpdateShards( value );
+    }
+
+    /// <summary>
+    /// Disable child gameobjects.
+    /// Use when collecting the shard.
+    /// </summary>
+    private void DisableModels() {
+        model.SetActive( false );
+    }
+}
