653010b23e3251aee91f891dc37535709f6611e3
davidguerreiro <me@davidguerreiro.com>
2020-09-05 15:51:08 +0100

    create object pool utility class

---
+++ Utilities/ObjectPool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+    public GameObject prefab;                               // Prefab to be used in the object pool.
+    public int poolSize;                                    // Size of the object pool.
+    private static List<GameObject> pool;                   // Object pool list.
+
+    /// <summary>
+    /// Set up object pool.
+    /// </summary>
+    private void SetUpPool() {
+
+        if ( pool == null ) {
+            pool =  new List<GameObject>();
+        }
+
+        for ( int i = 0; i < poolSize; i++ ) {
+            GameObject prefabObject = Instantiate( prefab );
+            prefabObject.SetActive( false );
+
+            pool.Add( prefabObject );
+        }
+    }
+}
