27838d18931efa720892dbc2e350e75e16c2d2a4
davidguerreiro <me@davidguerreiro.com>
2020-09-05 16:05:23 +0100

    complete object pool utility class

--- Utilities/ObjectPool.cs
+++ Utilities/ObjectPool.cs
@@ -19,9 +19,31 @@ public class ObjectPool : MonoBehaviour {
 
         for ( int i = 0; i < poolSize; i++ ) {
             GameObject prefabObject = Instantiate( prefab );
+            prefabObject.transform.parent = transform;
             prefabObject.SetActive( false );
 
             pool.Add( prefabObject );
         }
+    }
+
+    /// <summary>
+    /// Spawn prefab in the game scene.
+    /// </summary>
+    /// <param name="location">Vector3 - prefab location to be enabled</param>
+    /// <returns>GameObject</returns>
+    public GameObject SpawnPrefab( Vector3 location = new Vector3() ) {
+
+        foreach ( GameObject prefab in pool ) {
+
+            if ( prefab.activeSelf == false ) {
+
+                prefab.SetActive( true );
+                prefab.transform.localPosition = location;
+
+                return prefab;
+            }
+        }
+
+        return null;
     }
 }
