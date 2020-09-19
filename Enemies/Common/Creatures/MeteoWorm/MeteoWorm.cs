a18e3e86bb1e51c261bda06891b1b049bfa6c11b
davidguerreiro <me@davidguerreiro.com>
2020-09-19 20:18:15 +0100

    add enemy base class and methods to both enemy base class and meteo worm enemy class

--- Enemies/Common/Creatures/MeteoWorm/MeteoWorm.cs
+++ Enemies/Common/Creatures/MeteoWorm/MeteoWorm.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeteoWorm : MonoBehaviour
-{
+public class MeteoWorm : Enemy {
+
+    private Animator _anim;                             // Animator component reference.
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        Init();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    public override void Init() {
+        base.Init();
+
+        // get animator component reference.
+        _anim = GetComponent<Animator>();
     }
 }
--- Enemies/Enemy.cs
+++ Enemies/Enemy.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public abstract class Enemy : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Header("Data Source")]
+    [SerializeField]
+    protected EnemyData data;                                       // Enemy data scriptable object. Contains all this enemy default values.
+    protected float currentHp;                                      // Current enemy HP.
+    protected float maxHP;                                          // Max enemy hp.
+
+    [Header("Status")]
+    public bool isAlive = true;                             // Whether the enemy is alive or has already died.
+
+    public enum State {
+        watching,                                           // Enemy does not move, just observe the enviroment.
+        patrolling,                                         // Enemy is patrolling an area.
+        combat,                                             // Enemy is enaged in combat withing the player or another target.
+    };
+
+    public State currentState = new State();                // Enemy state.
+
+    // TODO: Add reference to UI component.
+
+    /// <summary>
+    /// Get damage method.
+    /// </summary>
+    /// <param name="externalImpactValue">float - damage value caused external attacker, usually the player.</param>
+    public virtual void GetDamage( float externalImpactValue ) {
+        float damageReceived = ( externalImpactValue / data.defense ) + UnityEngine.Random.Range( 0f, 2f );
+        Debug.Log( damageReceived );
+    }
+    
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    public virtual void Init() {
+        currentHp = data.hp;
+        maxHP = data.hp;
     }
 }
