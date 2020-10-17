0b63eac9f316f1ed62c69487913f773afc3ab18a
David Guerreiro <me@davidguerreiro.com>
2020-10-17 12:44:33 +0200

    add stop moving against obstacles for enemies

--- Player/Movement/FPSInput.cs
+++ Player/Movement/FPSInput.cs
@@ -110,7 +110,6 @@ public class FPSInput : MonoBehaviour {
         // check invencible counter if the player is invencible.
         if ( invencible ) {
              UpdateInvencible();
-             var x = 1;
         }
     }
 
