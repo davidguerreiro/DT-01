924e728adc339ae60d1214404d2ff3cccb501fc6
David Guerreiro <me@davidguerreiro.com>
2020-11-01 02:20:53 +0100

    add usable item parent class

---
+++ Interactables/Items/UsableItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UsableItem : MonoBehaviour {
+    protected Coroutine useCoroutine;                               // Use action coroutine reference.
+
+    /// <summary>
+    /// Use item action.
+    /// </summary>
+    /// <returns>void</returns>
+    public abstract void Use();
+
+    /// <summary>
+    /// Use item action coroutine.
+    /// Item use logic goes here.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    protected abstract IEnumerator UseRoutine();
+}
