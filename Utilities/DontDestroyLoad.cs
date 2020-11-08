09e5471b0e0b6482985dfb2c9cd3cd1363aeeaf6
David Guerreiro <me@davidguerreiro.com>
2020-11-08 18:07:01 +0100

    add utils class dont destroy on load

---
+++ Utilities/DontDestroyLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyLoad : MonoBehaviour {
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake() {
+        DontDestroyOnLoad( this.gameObject );    
+    }
+    
+}
