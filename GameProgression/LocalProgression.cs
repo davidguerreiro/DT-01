fa73c85377fda69cf6effc1b6877e6974ce958ec
David Guerreiro <me@davidguerreiro.com>
2020-11-09 14:03:05 +0100

    add local progression script class

---
+++ GameProgression/LocalProgression.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalProgression : MonoBehaviour {
+    public static LocalProgression instance;                    // Public static class instance.
+
+    [Header("Local Progression Data Source")]
+    public EnemyGroupsProgression enemyGroups;                  // Enemy groups progression data reference.
+}
