a7250f848b9447fd0504ad8552b42e35811c78d8
David Guerreiro <me@davidguerreiro.com>
2020-11-09 12:13:52 +0100

    complete local progression scriptable

---
+++ ScriptableObjects/GameProgression/LocalProgresionData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalProgresionData : ScriptableObject {
+    public int id;                                              // Local progression id.
+    public string sceneName;                                    // Local progression scene name.
+
+    public struct EnemyGroups {
+        EnemyGroup enemyGroup;                                  // Enemy group instance reference..
+        bool defeated;                                          // Flag to control if this enemy group has been defeated.
+    }
+
+    public EnemyGroups[] enemyGroups;                            // Array of enemy groups.
+
+}
