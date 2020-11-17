d2b82b5bc4eae18cb0c866c6759ee5763d260391
David Guerreiro <me@davidguerreiro.com>
2020-11-17 18:35:16 +0100

    create gravity crystal interactable class

---
+++ Interactables/Switchs/GravityCrystal/GravityCrystal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCrystal : MonoBehaviour {
+    public bool interactable;                                   // Whether this crystal is interactble or not.
+    public bool completeEnabled;                                // If true, this crystal has already been enabled as a switch.
+
+    private Animator _anim;                                     // Animator component refernece.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+
+        // get animator component.
+        _anim = GetComponent<Animator>();
+
+        if ( interactable ) {
+            _anim.SetBool( "Interactable", true );
+        }
+    }
+}
