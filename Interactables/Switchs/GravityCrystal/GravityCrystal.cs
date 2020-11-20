363548ae3f9616fb1dfd5f38250870cd1a1087f8
David Guerreiro <me@davidguerreiro.com>
2020-11-20 18:20:10 +0100

    add gravity crystal rotation elements code to gravirty crystal class

--- Interactables/Switchs/GravityCrystal/GravityCrystal.cs
+++ Interactables/Switchs/GravityCrystal/GravityCrystal.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityCrystal : MonoBehaviour {
+    [Header("Status")]
     public bool interactable;                                   // Whether this crystal is interactble or not.
     public bool completeEnabled;                                // If true, this crystal has already been enabled as a switch.
 
-    private Animator _anim;                                     // Animator component refernece.
+    [Header("Additional Elements")]
+    public RotateAround[] rotateElements;                       // Roteate around decorative elements.
 
-    // Start is called before the first frame update
+    [Header("Settings")]
+    public float activationHits;                                // How many hits this gravity cristal needs to release graviti energy.
+    public float minRotSpeed;                                   // Minumun rotate elements rotation speed.
+    public float maxRotSpeed;                                   // Maximun rotate elements rotation speed.
+
+    private Animator _anim;                                     // Animator component reference.
+
+    // Start is called before the first frame update.
     void Start() {
         Init();
+    }
+
+    /// <summary>
+    /// Set rotation elements speed.
+    /// </summary>
+    private void SetRotationalElementsSpeed() {
+        foreach ( RotateAround rotElement in rotateElements ) {
+            rotElement.speed = minRotSpeed;
+        }
     }
 
     /// <summary>
@@ -24,5 +42,8 @@ public class GravityCrystal : MonoBehaviour {
         if ( interactable ) {
             _anim.SetBool( "Interactable", true );
         }
+
+        // set initial rotation for elements.
+        SetRotationalElementsSpeed();
     }
 }
