af2b9707f1493a826ca7f51c75f7e5d9d110a0a2
David Guerreiro <me@davidguerreiro.com>
2020-12-13 17:37:39 +0100

    feat: complete exp bar class

--- UI/GamePlay/WeaponSection/ExpCircleBar.cs
+++ UI/GamePlay/WeaponSection/ExpCircleBar.cs
@@ -23,15 +23,31 @@ public class ExpCircleBar : MonoBehaviour {
     }
 
     /// <summary>
-    /// Set up exp level
+    /// Update exp
     /// bar.
     /// </summary>
+    /// <param name="currentExp">int - weapon current experience.</param>
+    public void UpdateExpBar( int currentExp ) {
+        float fCurrentExp = (float) currentExp;
+
+        float normalized = Utils.instance.Normalize( fCurrentExp, 0f, nextLevelExp );
+        _bar.fillAmount = normalized;
+    }
+
+    /// <summary>
+    /// Set up exp level
+    /// bar. This method must be called
+    /// when the scene is intialised or
+    /// player switchs weapon.
+    /// </summary>
+    /// <param name="currentExp">int - weapon current experience.</param>
+    /// <param name="nextLevelExp">int - weapon next level exp required</param>
     public void SetUp( int currentExp, int nextLevelExp ) {
         float fCurrentExp = (float) currentExp;
         this.nextLevelExp = (float) nextLevelExp;
 
         float normalized = Utils.instance.Normalize( fCurrentExp, 0f, this.nextLevelExp );
-//         _bar.
+        _bar.fillAmount = normalized;
     }
 
     /// <summary>
