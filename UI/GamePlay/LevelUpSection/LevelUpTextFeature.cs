a69941babef664d9ea5111fa983c917a99e5b906
David Guerreiro <me@davidguerreiro.com>
2020-12-23 19:03:22 +0100

    feat: add level up text feature class

---
+++ UI/GamePlay/LevelUpSection/LevelUpTextFeature.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpTextFeature : MonoBehaviour {
+    private TextComponent _text;                                  // Text component reference.
+    private Animator _anim;                                       // Animator component refernece.
+
+    // Start is called before the first frame update
+    void Start() {
+        Init();
+    }
+
+    /// <summary>
+    /// Init class method.
+    /// </summary>
+    private void Init() {
+
+        // get text component reference.
+        _text = GetComponent<TextComponent>();
+
+        // get animator component reference.
+        _anim = GetComponent<Animator>();
+    }
+}
