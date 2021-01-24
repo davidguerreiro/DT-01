b4da45e4bc55a80948046c12403b3899c55177cf
David Guerreiro <me@davidguerreiro.com>
2021-01-24 19:39:40 +0100

    feat: add level music manager script

--- GameManagers/LevelManagers/LevelManager.cs
+++ GameManagers/LevelManagers/LevelManager.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
-{
+public class LevelManager : MonoBehaviour {
+    public static LevelManager instance;                        // Class static instance.
+    public LevelMusicController levelMusicController;           // Level music controller reference.
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake() {
+        if ( instance == null ) {
+            instance = this;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
