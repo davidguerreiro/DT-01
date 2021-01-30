c369ba22c281d489070dbf528b34733b616bd0f3
David Guerreiro <me@davidguerreiro.com>
2021-01-30 17:34:58 +0100

    feat: add area loaders to improve game performance

---
+++ GameProgression/AreaLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaLoader : MonoBehaviour {
+    
+    public GameObject[] objectsToLoad;                          // Objects to load in the scene.
+    public GameObject[] objectsToUnload;                        // Objects to unload in the scene.
+
+    /// <summary>
+    /// Load and unload objects.
+    /// </summary>
+    private void LoadUnloadObjects() {
+        foreach ( GameObject element in objectsToLoad ) {
+            element.SetActive(true);
+        }
+        
+        foreach( GameObject element in objectsToLoad ) {
+            element.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            LoadUnloadObjects();
+        }
+    }
+}
