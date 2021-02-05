a0bb7622daa052708e46ba302398f26234116588
David Guerreiro <me@davidguerreiro.com>
2021-02-05 23:05:49 +0100

    feat: add base gravity rocks cinematic script

---
+++ Cinematic/Cinematics/CrystalCaves/GravityRocks/GravityRocksCinematic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityRocksCinematic : Cinematic {
+
+    public PasiveGravityCristal gravityCrystal;             // Gravity cristal used in the animation.
+    public Animator[] rocks;                                // Animation rocks.
+
+    /// <summary>
+    /// Play cinematic.
+    /// </summary>
+    public override void PlayCinematic() {
+        if ( cinematicRoutine == null ) {
+            cinematicRoutine = StartCoroutine(PlayCinematicRoutine());
+        }
+    }
+
+    /// <summary>
+    /// Play cinematic coroutine.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    protected override IEnumerator PlayCinematicRoutine() {
+        inProgress = true;
+        base.StartInGame();
+        yield return null;
+    }
+}
