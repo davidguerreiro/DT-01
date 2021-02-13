c0a41787beb81c3c909d4ce6bf82b6fdec911c16
David Guerreiro <me@davidguerreiro.com>
2021-02-13 14:15:46 +0100

    feat: complete actor name dialogue class

---
+++ UI/Dialogue/DialogueActorName.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueActorName : MonoBehaviour {
+    public TextComponent actorName;                         // Actor name text component.
+    public FadeElement actorFade;                           // Actor fade component.
+
+    /// <summary>
+    /// Display actor name.
+    /// </summary>
+    /// <param name="name">string - actorName. Empty to keep previous value</param>
+    public void Display( string name = "" ) {
+        if ( name != "" ) {
+            actorName.UpdateContent(name);
+        }
+
+        actorFade.FadeIn();
+    }
+
+    /// <summary>
+    /// Clear actor name.
+    /// </summary>
+    /// <param name="removeName">bool - if true, removes the current actor name text value. Default to true.</param>
+    public void Clear( bool removeName = true ) {
+        StartCoroutine(ClearRoutine(removeName));
+    }
+
+    /// <summary>
+    /// Clear actor name coroutine.
+    /// </summary>
+    /// <param name="removeName">bool - if true, removes the current actor name text value.</param>
+    /// <return>IEnumerator</return>
+    public IEnumerator ClearRoutine( bool removeName ) {
+        actorFade.FadeOut();
+        yield return new WaitForSeconds(1f);
+
+        if (removeName) {
+            actorName.UpdateContent("");
+        }
+    }
+}
---
+++ UI/Dialogue/DialogueText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueText : MonoBehaviour {
+
+    public bool InDialogue;                                 // Flag to control whether dialogue is displayed.
+
+    [Header("Data Source")]
+    public DialogueContent content;                         // Dialogue content.
+
+    [Header("Components")]
+    public TextComponent actorName;                         // Actor name component.
+    public TextComponent dialogueText;                      // Dialogue text component.
+    public Animator anim;                                   // Animator component reference.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
