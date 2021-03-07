de1d6c3ea4dc1997e1d9c8184c899ba8b6752d79
David Guerreiro <me@davidguerreiro.com>
2021-03-07 17:03:58 +0100

    feat: add base intro cinematic script and camera animatios

--- Cinematic/Cinematics/MainCinematics/01AComvenientAlliance/IntroCinematicOne.cs
+++ Cinematic/Cinematics/MainCinematics/01AComvenientAlliance/IntroCinematicOne.cs
@@ -8,9 +8,15 @@ public class IntroCinematicOne : Cinematic {
     public GameObject landingSmoke;                             // Pirate's ship landing smoke effect.
     public CinematicElementGenerator generator;                 // Landing area's generator.
 
+    [Header("Main Actors")]
+    public Animator spaceShip;                                  // Pirates spaceship anim reference.
+    public Animator probotLeft;                                 // Left Probot anim.
+    public Animator probotRight;                                // Left Probot anim.
+
+
     // Start is called before the first frame update
     void Start() {
-        // TOOD: Call init cinematic here.
+        PlayCinematicRoutine();
     }
 
     /// <summary>
@@ -28,9 +34,35 @@ public class IntroCinematicOne : Cinematic {
     /// <returns>IEnumerator</returns>
     protected override IEnumerator PlayCinematicRoutine() {
         inProgress = true;
+        yield return new WaitForSeconds(1f);
+
+        // play music.
+        LevelManager.instance.levelMusicController.PlaySong("scene", "introMusic");
+        yield return new WaitForSeconds(1f);
+
+        // play camera animations and remove cinematic UI cover.
+        cameras[0].PlayBoolAnim("Intro1Shoots", true);
+        CinematicUI.instance.cover.FadeOut();
+        yield return new WaitForSeconds(2f);
+
+        // play generator animations.
+        generator.audio.PlaySound();
+        generator.anim.SetTrigger("StartEngine");
+        yield return new WaitForSeconds(2f);
+
+        generator.RotateFan();
+        yield return new WaitForSeconds(1.5f);
+        generator.sparksAudio.PlaySound(0);
+        generator.sparks.Play();
+        yield return new WaitForSeconds(.5f);
+        generator.sparks.Stop();
+        yield return new WaitForSeconds(1.5f);
+        generator.sparksAudio.PlaySound(1);
+        generator.sparks.Play();
+
         inProgress = false;
         cinematicRoutine = null;
-        yield break;
+        
     }
 
 }
