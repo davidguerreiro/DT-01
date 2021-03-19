287537a1ffff3ad49822cf8fc7d9287bf8dd3bd7
David Guerreiro <me@davidguerreiro.com>
2021-03-19 13:43:13 +0100

    feat: add secondary animator to actor generic

--- Cinematic/Actors/ActorGeneric.cs
+++ Cinematic/Actors/ActorGeneric.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 
 public class ActorGeneric : Actor {
     public string actorNameId;                  // Actor name id.
+    public Animator secondaryAnimator;          // Secondary animator component, usually moving/action root animator.
 }
